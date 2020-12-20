d4c5add6df566813d3b50aed94bcf304b5986292
CodeLyoko2610 <dangchuong1999@outlook.com>
2020-12-21 01:28:31 +0200

    Connect to mssql database. Create table Book using the Book model, add-migration and update-database commands of Entity Framework.

---
+++ BookListRazor1/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor1.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Entities
+        public DbSet<Book> Book { get; set; }
+    }
+}
