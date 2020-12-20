d0f14f47b11995ba28101e95620e9fefeb9432d7
CodeLyoko2610 <dangchuong1999@outlook.com>
2020-12-21 00:36:19 +0200

    Create model for book.

---
+++ BookListRazor1/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor1.Models
+{
+    public class Book
+    {
+        [Key] //primary key, created automatically
+        public int Id { get; set; }
+               
+        [Required] //cannot be null
+        public string Name { get; set; }
+
+        public int Author { get; set; }
+
+
+    }
+}
